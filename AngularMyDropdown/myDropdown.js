7e8c6c3de89fafef8136c58f6d433a35f500f54e
kaushalpapers <kaushal.papers@gmail.com>
2017-05-15 13:15:51 -0400

    Added dropdown

---
+++ AngularMyDropdown/myDropdown.js
@@ -0,0 +1,107 @@
+function myDropdown() {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var list = element.find('[my-list]');
+            var txtbox = element.find('[my-textbox]');
+            var listItemHoverClass = list.attr('my-list-item-hover-class');
+
+            //add below sectiont till end
+             if(txtbox.prop("tagName") != 'input') 
+             {
+                 txtbox.focus(function(){
+                     list.show();
+                 });
+                  txtbox.click(function(){
+                     list.show();
+                 });
+             }
+             //end
+
+            list.hide();
+            AddTabEvent();
+
+            list.bind('click', function () {
+                setValue();
+            });
+
+            list.hover(function () {
+                var listItems = list.find('[my-list-item]');
+                listItems.hover(function () {
+                    listItems.removeClass(listItemHoverClass);
+                    $(this).addClass(listItemHoverClass);
+                }, function () {
+                    $(this).removeClass(listItemHoverClass);
+                });
+
+            }, function () {
+                var listItems = list.find('my-list-item');
+                listItems.unbind('hover');
+            });
+
+            txtbox.bind('keyup', function (event) {
+                list.show();
+                var selectedItem = list.find('[my-list-item]').filter('.' + listItemHoverClass);
+                if (event.keyCode == 40) //down 
+                {
+                    if (selectedItem.length > 0) {
+                        var next = selectedItem.next();
+                        if (next.length > 0) {
+                            next.addClass(listItemHoverClass);
+                            selectedItem.removeClass(listItemHoverClass);
+                        }
+                    } else
+                        list.find('[my-list-item]').first().addClass(listItemHoverClass);
+
+                } else if (event.keyCode == 38) {
+                    if (selectedItem.length > 0) {
+                        var prev = selectedItem.prev();
+                        if (prev.length > 0) {
+                            prev.addClass(listItemHoverClass);
+                            selectedItem.removeClass(listItemHoverClass);
+                        }
+                    } 
+                } else if (event.keyCode == 13) {
+                     triggerClick();
+                }
+                else if (event.keyCode == 9) {
+                     triggerClick();
+                }
+            })
+
+            //private functions
+            function triggerClick(){
+                var selectedItem = list.find('[my-list-item]').filter('.' + listItemHoverClass);
+                if (selectedItem.length > 0) {
+                    selectedItem.trigger('click');
+                }
+            }
+
+            function setValue() {
+                var selectedItem = list.find('[my-list-item]').filter('.' + listItemHoverClass);
+                if (selectedItem.length > 0) {
+                    var item = selectedItem.find('[my-list-item-value]');
+                    var itemValue = item.length > 0 ? item.text() : selectedItem.text()
+                    if(txtbox.prop("tagName") != 'input') //add
+                        txtbox.html(itemValue);//add
+                    else//add
+                        txtbox.val(itemValue);
+
+                    list.hide();
+                }
+            }
+
+            function AddTabEvent(){
+                $(document).on('keyup', function(event){
+                    if(event.keyCode == 9)
+                       triggerClick();
+                });
+            }
+
+        }
+    }
+};
+
+
+var app = angular.module("main", []);
+app.directive("myDropdown", myDropdown);
